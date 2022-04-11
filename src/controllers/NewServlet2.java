a73e2c91406998db3634554f92b2a823400e2123
01048402 <01048402@YE6056.ancor2.r.recruit.co.jp>
2022-04-11 14:57:03 +0900

    newまで作成

--- src/controllers/NewServlet2.java
+++ src/controllers/NewServlet2.java
@@ -1,8 +1,9 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 
-import javax.servlet.RequestDispatcher;
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,18 +11,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
  */
 @WebServlet("/new")
-public class NewServlet extends HttpServlet {
+public class NewServlet2 extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public NewServlet() {
+    public NewServlet2() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,13 +33,25 @@ public class NewServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
-        request.setAttribute("_token", request.getSession().getId());
+        EntityManager em = DBUtil.createEntityManager();
+        em.getTransaction().begin();
 
-        request.setAttribute("tasks", new Task());
+        Task task = new Task();
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
-        rd.forward(request, response);
+        String content = "hello";
+        task.setContent(content);
 
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        task.setCreated_at(currentTime);
+        task.setModify_at(currentTime);
+
+        em.persist(task);
+        em.getTransaction().commit();
+
+
+        response.getWriter().append(Integer.valueOf(task.getId()).toString());
+
+        em.close();
     }
 
 }
